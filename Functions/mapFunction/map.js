d9fb45b907328b7f55549160a62e67c38ff44b4d
ashu0831 <ashunegi0831@gmail.com>
2024-07-26 11:46:33 +0530

    updated

--- Functions/mapFunction/map.js
+++ Functions/mapFunction/map.js
@@ -31,5 +31,76 @@ const largestNumber = Math.max(...number);
 console.log(largestNumber);
 
 
+//return a array and return all the element greater than 50
+
+let A = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+A.map((a) => {
+  if (a > 50) {
+    console.log(a);
+  }
+});
+
+//greatest in the array
+
+const numbe = [10, 20, 30, 40, 50];
+
+let greatestNumber = numbe[0];
+
+numbe.map((num) => {
+  if (num > greatestNumber) {
+    greatestNumber = num;
+  }
+});
+
+console.log(greatestNumber);
+
+
+const person = [
+  {
+    id: 1,
+    first_name: "Nickie",
+    last_name: "Southorn",
+    email: "nsouthorn0@t.co",
+    gender: "Male",
+    phone: "+86 988 458 8839",
+  },
+  {
+    id: 2,
+    first_name: "Priya",
+    last_name: "Guyet",
+    email: "pguyet1@psu.edu",
+    gender: "Male",
+    phone: "+225 201 122 8186",
+  },
+  {
+    id: 3,
+    first_name: "Neda",
+    last_name: "Chapell",
+    email: "nchapell2@chicagotribune.com",
+    gender: "Female",
+    phone: "+55 296 146 2266",
+  },
+  {
+    id: 4,
+    first_name: "Dewie",
+    last_name: "Argontt",
+    email: "dargontt3@umich.edu",
+    gender: "Male",
+    phone: "+62 933 617 6542",
+  },
+  {
+    id: 5,
+    first_name: "Cloe",
+    last_name: "Oakshott",
+    email: "coakshott4@about.me",
+    gender: "Female",
+    phone: "+1 864 342 0870",
+  },
+];
+
+const firstNames = person.map((p) => p.first_name);
+
+console.log(firstNames);
+
 
 
